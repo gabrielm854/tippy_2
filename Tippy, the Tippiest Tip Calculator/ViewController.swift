9d302f1f1a3ddd1521e373738e5e23e60cde4206
Gabriel Muniz <gabrielm854@fb.com>
2017-06-20 13:27:47 -0700

    Initial Commit

---
+++ Tippy, the Tippiest Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy, the Tippiest Tip Calculator
+//
+//  Created by Gabriel Muñiz on 6/20/17.
+//  Copyright © 2017 Gabriel Muñiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
